1a7df016c62263c6b26a70ea6cf98ae75a218885
andrewskr90 <andrewskr90@gmail.com>
2022-11-29 10:56:46 -0800

    installed react dependencies, webpack, and babel. Set up basic nav bar and body

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='app'>
+            <header className='navBar'>
+                <h2 className='title'>Voice Of Vashon</h2>
+                <nav className='navigation'>
+                    <a>One</a>
+                    <a>Two</a>
+                    <a>Three</a>
+                    <a>Four</a>
+                </nav>
+            </header>
+            <body>
+                <section>
+                    
+                </section>
+                <section>
+                    
+                </section>
+                <section>
+                    
+                </section>
+                <section>
+                    
+                </section>
+                <section>
+                    
+                </section>
+                <section>
+                    
+                </section>
+
+            </body>
+        </div>
+    )
+}
+
+export default App
